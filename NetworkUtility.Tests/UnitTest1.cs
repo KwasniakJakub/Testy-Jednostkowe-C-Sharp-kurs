b9a15291c527ac807ad62633d2beb78a723199b3
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-08-05 00:40:36 +0200

    Xunit and Fluent Assertion

---
+++ NetworkUtility.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace NetworkUtility.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]//Fact/Test
+        public void Test1()
+        {
+
+
+            //Assert.Pass();
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,16 @@
+﻿namespace NetworkUtility.Ping;
+
+public class NetworkService
+{
+    public string SendPing()
+    {
+        //SearchDNS();
+        //BuildPacket();
+        return "Success: Ping Sent!";
+    }
+
+    public int PingTimeout(int a, int b)
+    {
+        return a + b;
+    }
+}
